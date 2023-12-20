b7394498e004bd7e2015b692d785e44800dd9894
stefannyaonofre <sonofreg@uqvirtual.edu.co>
2023-12-20 16:17:57 -0500

    creaction register and login, firebase

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Router from './router/Router.jsx'
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Provider } from 'react-redux';
+import { store } from './store/store.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <Router />,
+    <Provider store={store}>
+        <Router />
+    </Provider>,
 )
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
+import "./login.scss";
+import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { useDispatch } from "react-redux";
+import { loginWithEmailAndPassword } from "../../store/auth/authThunk";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
 
-export default Login
+  const {register, handleSubmit} = useForm();
+  const dispatch = useDispatch();
+
+  const onSubmit = (data) => {
+    dispatch(loginWithEmailAndPassword(data));
+  }
+
+  return (
+    <main className="main__login">
+      <div className="main__login__info">
+        <figure className="main__login__logo">
+          <img src="https://png.pngtree.com/png-vector/20191110/ourmid/pngtree-avatar-icon-profile-icon-member-login-vector-isolated-png-image_1978396.jpg" />
+        </figure>
+        <form className="main__login__form" onSubmit={handleSubmit(onSubmit)}>
+        <h4>Iniciar Sesión</h4>
+        <input type="text" className="form-control" placeholder="user@gmail.com" {...register("email", { required: true })} />
+        <input type="password" className="form-control" placeholder="Contraseña" {...register("password", { required: true })} />
+        <button type="submit" className="main__login__button">Iniciar Sesión</button>
+        <Link to={'/register'}>Registrase</Link>
+        </form>
+      </div>
+    </main>
+  );
+};
+
+export default Login;
